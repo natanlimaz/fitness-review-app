52a5770c707409ad99cf02458474eca3054b2feb
natanlimaz <natanlimadev@gmail.com>
2023-01-12 14:01:58 -0300

    :brain: implementation of logical parts

--- app/src/main/java/co/natanlima/fitnessreview/ImcActivity.kt
+++ app/src/main/java/co/natanlima/fitnessreview/ImcActivity.kt
@@ -2,10 +2,35 @@ package co.natanlima.fitnessreview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class ImcActivity : AppCompatActivity() {
+
+    lateinit var editWeight: EditText
+    lateinit var editHeight: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_imc)
+
+        editWeight = findViewById(R.id.edit_imc_weight)
+        editHeight = findViewById(R.id.edit_imc_height)
+
+        val btnSend: Button = findViewById(R.id.btn_imc_send)
+        btnSend.setOnClickListener {
+            if(!validate()){
+                Toast.makeText(this, R.string.fields_messages, Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            //sucess
+        }
+    }
+
+    private fun validate(): Boolean {
+        // nao pode inserir valores nulos / vazio
+        // nao pode inserir / comecar com 0
+        return editWeight.text.toString().isNotEmpty() && editHeight.text.toString().isNotEmpty() && !editWeight.text.toString().startsWith("0") && !editHeight.text.toString().startsWith("0")
     }
 }
