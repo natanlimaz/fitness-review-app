f89de201e52b33e49a0dc08638287b388df4165f
natanlimaz <natanlimadev@gmail.com>
2023-01-13 09:53:14 -0300

    :thought_balloon: code refactoring and implementation of logical parts

--- app/src/main/java/co/natanlima/fitnessreview/ImcActivity.kt
+++ app/src/main/java/co/natanlima/fitnessreview/ImcActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.annotation.StringRes
 
 class ImcActivity : AppCompatActivity() {
 
@@ -32,6 +33,23 @@ class ImcActivity : AppCompatActivity() {
             val imcResult = calculateImc(weight, height)
             Log.d("Teste", "Resultado: $imcResult")
 
+            val imcResponseId = imcResponse(imcResult)
+            Toast.makeText(this, imcResponseId, Toast.LENGTH_SHORT).show()
+
+        }
+    }
+
+    @StringRes
+    private fun imcResponse(imc: Double): Int{
+        return when {
+            imc < 15.0 -> R.string.imc_severy_low_weight
+            imc < 16.0 -> R.string.imc_very_low_weight
+            imc < 18.5 -> R.string.imc_low_weight
+            imc < 25.0 -> R.string.normal
+            imc < 30.0 -> R.string.imc_high_weight
+            imc < 35.0 -> R.string.imc_so_high_weight
+            imc < 40.0 -> R.string.imc_severely_high_weight
+            else -> R.string.imc_extreme_weight
         }
     }
 
