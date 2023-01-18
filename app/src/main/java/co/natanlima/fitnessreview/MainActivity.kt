aa767fa7bf5a761a7a44fc676bd9a8d77e9b078b
natanlimaz <natanlimadev@gmail.com>
2023-01-18 01:15:56 -0300

    applying of recyclerView concept in app

--- app/src/main/java/co/natanlima/fitnessreview/MainActivity.kt
+++ app/src/main/java/co/natanlima/fitnessreview/MainActivity.kt
@@ -1,26 +1,107 @@
 package co.natanlima.fitnessreview
 
-import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnImc: LinearLayout // variavel de campo - escopo;campo da classe (que irei inicializar depos)
+    //private lateinit var btnImc: LinearLayout // variavel de campo - escopo;campo da classe (que irei inicializar depos)
+    private lateinit var recyclerViewMain: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnImc = findViewById(R.id.btn_imc)
+        val mainItems = mutableListOf<MainItem>()
+        mainItems.add(
+            MainItem(
+                id = 1,
+                drawableId = R.drawable.ic_baseline_wb_sunny_24,
+                textStringId = R.string.label_imc,
+                color = Color.GREEN
+            )
+        )
 
-        /*
-            Intent -> intencao de voce abrir algum recurso do sistema operacional android (pode ser varias coisas: link/atividade/telefonema (serviços do sistema))
-        * */
-        btnImc.setOnClickListener {
-            val i = Intent(this, ImcActivity::class.java) //ctx de execucao atividade corrente, classe q a gente quer inicializar
-            startActivity(i)
+        mainItems.add(
+            MainItem(
+                id = 2,
+                drawableId = R.drawable.baseline_visibility_24,
+                textStringId = R.string.label_tmb,
+                color = Color.YELLOW
+            )
+        )
+
+        mainItems.add(
+            MainItem(
+                id = 3,
+                drawableId = R.drawable.baseline_visibility_24,
+                textStringId = R.string.app_name,
+                color = Color.MAGENTA
+            )
+        )
+
+        // 1) o layout xml
+        // 2) a onde a recyclerView vai aparecer (tela principal, tela cheia)
+        // 3) logica - conectar o xml da celula DENTRO do recyclerView + a sua quantidade de elementos dinâmicos
+        val adapter = MainAdapter(mainItems)
+        recyclerViewMain = findViewById(R.id.rv_main) // referencia da RecyclerView
+        recyclerViewMain.adapter = adapter
+
+        recyclerViewMain.layoutManager = LinearLayoutManager(this) // COMPORTAMENTO DA NOSSA LISTAGEM
+
+//        btnImc = findViewById(R.id.btn_imc)
+//
+//        /*
+//            Intent -> intencao de voce abrir algum recurso do sistema operacional android (pode ser varias coisas: link/atividade/telefonema (serviços do sistema))
+//        * */
+//        btnImc.setOnClickListener {
+//            val i = Intent(this, ImcActivity::class.java) //ctx de execucao atividade corrente, classe q a gente quer inicializar
+//            startActivity(i)
+//        }
+    }
+
+    private inner class MainAdapter(private val mainItems: List<MainItem>) : RecyclerView.Adapter<MainViewHolder>() {
+        // 1 - Qual é o layout XML da célula específica (item)
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+
+            val view = layoutInflater.inflate(R.layout.main_item, parent, false)
+
+            return MainViewHolder(view)
+
+        }
+
+        // 2 - Disparado toda vez que houver uma rolagem na tela e for necessário trocar o conteúdo da célula
+        override fun getItemCount(): Int {
+            return mainItems.size
+        }
+
+        // 3 - Informar quantas células essa listagem terá
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            val itemCurrent = mainItems[position]
+            holder.bind(itemCurrent)
+        }
+
+    }
+
+    // é a classe da célula em si!!!
+    private class MainViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: MainItem) {
+            val img: ImageView = itemView.findViewById(R.id.item_img_icon)
+            val name: TextView = itemView.findViewById(R.id.item_txt_name)
+            val container: LinearLayout = itemView as LinearLayout
+
+            img.setImageResource(item.drawableId)
+            name.setText(item.textStringId)
+            container.setBackgroundColor(item.color)
         }
     }
 }
